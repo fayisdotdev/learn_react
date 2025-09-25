df0aef112edd62555565b5d6be672d5db2c530be
fayizhere <fayizmfm@gmail.com>
2025-09-25 22:53:19 +0530

    counter app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-function increment() {
-  console.log("Increment");
-}
+import { useState } from "react";
+
 export default function App() {
+  const increment = () => {
+    setCount(count + 1);
+    console.log("Count is: ", count);
+  };
+  const [count, setCount] = useState(0); // useState is a Hook that lets you add React state to function components. setState is a function that lets you update the state. You can pass an initial state to useState. The initial state is 0 in this case.
   return (
     <div>
-      <h1>0</h1>
-      <button onClick={increment}>+</button>
+      <h2>Hi Babyyy</h2>
+      <h2>This is a react Counter App</h2>
+      <h2>You clicked {count} times</h2>
+      <h2>
+        You can click here <button onClick={increment}>+</button>{" "}
+      </h2>
+      {/* <h1>{count}</h1> */}
     </div>
   );
 }
