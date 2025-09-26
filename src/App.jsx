b423bbc1561f48980d26d780ed17f3eb145c6b20
fayizhere <fayizmfm@gmail.com>
2025-09-26 16:54:05 +0530

    see capitals of state - useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import StudnetsComponent from "./components/StudentsComponent.jsx";
+import { useState } from "react";
 
 export default function App() {
-  const datum = [
-    {
-      name: "Fayis",
-      description: "learning react",
-    },
-    {
-      name: "Shahal",
-      description: "Data Scientist",
-    },
-    {
-      name: "Shafna",
-      description: "UI UX Designer",
-    },
-  ];
+
+  const data = {
+    Kerala : "Thiruvananthapuram",
+    TamilNadu : "Chennai",
+    Karnataka : "Bangalore",
+    AndhraPradesh : "Amaravati",
+    Mumbai : "Maharashtra",
+    Goa : "Panaji",
+    Gujarat : "Gandhinagar" 
+  }
+
+  const [selectedState, setSelectedState] = useState("Kerala")
+  const [selectedStateCapital, setSelectedStateCapital] = useState("Thiruvananthapuram")
+
+  const changeStateName = (event) => {setSelectedState(event.target.value); setSelectedStateCapital(data[event.target.value])}
+
   return (
     <div>
-      <h2>Student Details</h2>
-      <hr />
-      {datum.map((showDetails) => (
-        <StudnetsComponent
-          name={showDetails.name}
-          description={showDetails.description}
-        />
-        
-      ))}
-      <hr/>
+      <select onChange = {changeStateName}>
+        <option value="Kerala">Kerala</option>
+        <option value="TamilNadu">TamilNadu</option>
+        <option value="Karnataka">Karnataka</option>
+        <option value="AndhraPradesh">AndhraPradesh</option>
+        <option value="Mumbai">Mumbai</option>
+        <option value="Gujarat">Gujarat</option>
+        <option value="Goa">Goa</option>
+      </select>
+      <p>Selected: {selectedState} </p>
+      <p>Capital of {selectedState} is {selectedStateCapital}.</p>
     </div>
   );
 }
--- src/components/StudentsComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function StudnetsComponent({ name, description }) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <h3>{description}</h3>
-    </div>
-  );
-}
