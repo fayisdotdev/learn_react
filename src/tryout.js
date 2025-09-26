2bfc9f0a5276b5331cd9746a695b9efa1e94aca9
fayizhere <fayizmfm@gmail.com>
2025-09-26 15:03:13 +0530

    cube tryout

--- src/tryout.js
+++ src/tryout.js
@@ -1,6 +1,6 @@
 const numbers = [1,2,3,4,5];
 
-const  squareFunction = (val) => val * val;
+const  squareFunction = (val) => val * val * val;
 
 // creating another array using teh exisnti9ing array and the defined function 
 const squares = numbers.map(squareFunction)
