920fdad998f155edb8e749b1197f4f9842549a50
fayizhere <fayizmfm@gmail.com>
2025-09-26 16:01:19 +0530

    maps in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from "react";
+import StudnetsComponent from "./components/StudentsComponent.jsx";
 
 export default function App() {
-  const increment = () => {
-    setCount(count + 1);
-    console.log("Count is: ", count);
-  };
-  const [count, setCount] = useState(0); // useState is a Hook that lets you add React state to function components. setState is a function that lets you update the state. You can pass an initial state to useState. The initial state is 0 in this case.
+  const datum = [
+    {
+      name: "Fayis",
+      description: "learning react",
+    },
+    {
+      name: "Shahal",
+      description: "Data Scientist",
+    },
+    {
+      name: "Shafna",
+      description: "UI UX Designer",
+    },
+  ];
   return (
     <div>
-      <h2>Hi Babyyy</h2>
-      <h2>This is a react Counter App</h2>
-      <h2>You clicked {count} times</h2>
-      <h2>
-        You can click here <button onClick={increment}>+</button>{" "}
-      </h2>
-      {/* <h1>{count}</h1> */}
+      <h2>Student Details</h2>
+      <hr />
+      {datum.map((showDetails) => (
+        <StudnetsComponent
+          name={showDetails.name}
+          description={showDetails.description}
+        />
+        
+      ))}
+      <hr/>
     </div>
   );
 }
---
+++ src/components/StudentsComponent.jsx
@@ -0,0 +1,8 @@
+export default function StudnetsComponent({ name, description }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{description}</h3>
+    </div>
+  );
+}
